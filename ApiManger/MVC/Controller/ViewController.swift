358cbf2377008269107ea9d167d39d157574045c
PG <prashantgajjar.work@gmail.com>
2022-11-18 01:29:28 +0530

    url task using JSONSerialization

--- ApiManger/MVC/Controller/ViewController.swift
+++ ApiManger/MVC/Controller/ViewController.swift
@@ -13,11 +13,39 @@ class ViewController: UIViewController {
     //MARK: - LifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
     }
-
-    //MARK: - Public Methods
     
     //MARK: - Private Methods
+    private func setup() {
+        let urlStrl = "https://pixabay.com/api/?key=\(constant.apiKey)&q=yellow+flowers&image_type=photo&pretty=true"
+        
+        guard let url = URL(string: urlStrl) else {
+            print("wrong url")
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            
+            guard let data = data else {
+                print("nil data")
+                return
+            }
+            
+            if let json = try? JSONSerialization.jsonObject(with: data) {
+                print(json)
+            }
+        }
+        task.resume()
+        
+    }
 
 }
 
